56899201207b235367abe7326d49cce93702f891
Ivan Gumeniuk <53488341+ivan-gumenuk@users.noreply.github.com>
2020-01-23 18:05:53 +0200

    update reading string
    сменил функцию getchar() на read()...

--- src/mx_read_input.c
+++ src/mx_read_input.c
@@ -1,7 +1,3 @@
-//
-// Created by Oleg Berdyshev on 1/22/20.
-//
-
 #include "ush.h"
 
 static void check_malloc(char *buffer);
@@ -9,17 +5,14 @@ static void check_malloc(char *buffer);
 char *mx_read_input(void) {
     int bufsize = 128;
     char *buffer = malloc(sizeof(char) * bufsize);
-    int c;
 
     check_malloc(buffer);
     for (int position = 0;; position++) {
-        c = getchar();
-        if (c == EOF || c == '\n') {
+        read(0, &buffer[position], 1);
+        if (buffer[position] == EOF || buffer[position] == '\n') {
             buffer[position] = '\0';
             return buffer;
         }
-        else
-            buffer[position] = c;
         if (position >= bufsize) {
             bufsize += 128; //If we have exceeded the buffer, reallocate.
             buffer = realloc(buffer, bufsize);
