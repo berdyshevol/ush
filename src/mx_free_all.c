9d8de09b972da1d7cf02200b9f66c293d84e4746
Ivan Gumeniuk <53488341+ivan-gumenuk@users.noreply.github.com>
2020-03-06 11:53:10 +0200

    Почистил лики в Jobs_list

--- src/mx_free_all.c
+++ src/mx_free_all.c
@@ -3,6 +3,22 @@
 //
 #include "ush.h"
 
+static void free_jobs_list(t_global_environment *gv) {
+    t_stoped *head = gv->jobs_list;
+    t_stoped *next;
+
+    while (head->next != NULL) {
+        mx_strdel(&head->str);
+        next = head->next;
+        free(head);
+        head = next;
+    }
+    if (head->str != NULL)
+        mx_strdel(&head->str);
+    free(head);
+}
+
 void mx_free_all(t_global_environment *gv) {
+    free_jobs_list(gv);
     free(gv);
 }
