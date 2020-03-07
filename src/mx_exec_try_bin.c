9bf2ec0fd82de65f8a98482658d4f8bd5900d873
Oleg Berdishev <berdyshevo@gmail.com>
2020-03-07 13:34:32 +0200

    try merge cmd substitution

--- src/mx_exec_try_bin.c
+++ src/mx_exec_try_bin.c
@@ -38,34 +38,12 @@ static void exec_child(char *cmd, t_global_environment *gv) {
     exit(0);    
 }
 
-static void add_process_list(t_global_environment *gv, pid_t pid) {
-    t_stoped *tmp = gv->jobs_list;
-
-    while (tmp->next != NULL) {
-        if (tmp->str == NULL) {
-            tmp->pid = pid;
-            tmp->str = strdup(gv->str);
-            gv->last_stoped = tmp;
-            printf("\n\r\x1b[2K[%d] %d suspended %s\n", tmp->n, pid, gv->str);
-            return;
-        }
-        tmp = tmp->next;
-    }
-    if (tmp->next == NULL) {
-        tmp->next = mx_add_empty_job(gv);
-        tmp->pid = pid;
-        tmp->str = strdup(gv->str);
-        gv->last_stoped = tmp;
-        printf("\n\r\x1b[2K[%d] %d suspended %s\n", tmp->n, pid, gv->str);
-    }
-}
-
 void mx_smart_wait(int pid, t_eval_result result, t_global_environment *gv) {
     int status;
 
     waitpid (pid, &status, WUNTRACED);
     if (MX_WIFSTOPPED(status)) {
-        add_process_list(gv, pid);
+        mx_add_process_list(gv, pid);
         gv->count_jobs++;
     }
     char *itoa = mx_itoa(mx_wexitstatud(status));
@@ -102,19 +80,30 @@ bool mx_try_bin(char *cmd, t_eval_result result, t_global_environment *gv) {
                 }
                 close(gv->cnf->pipe_fd[1]);
             }
-//            waitpid (pid, &status, WUNTRACED);
-//            if (MX_WIFSTOPPED(status)) {
-//                add_process_list(gv, pid);
-//                gv->count_jobs++;
-//            }
-//            char *itoa = mx_itoa(mx_wexitstatud(status));
-//            mx_env_set_var("?", itoa, &(gv->vars));
-//            result->status = mx_wexitstatud(status) == 0 ? true : false;
-//            tcsetpgrp(STDIN_FILENO, getpgrp());
-//            tcsetpgrp(STDOUT_FILENO, getpgrp());
-//            mx_strdel(&itoa);
             mx_smart_wait(pid, result, gv);
             break;
     }
     return true;
 }
+
+//static void add_process_list(t_global_environment *gv, pid_t pid) {
+//    t_stoped *tmp = gv->jobs_list;
+//
+//    while (tmp->next != NULL) {
+//        if (tmp->str == NULL) {
+//            tmp->pid = pid;
+//            tmp->str = strdup(gv->str);
+//            gv->last_stoped = tmp;
+//            printf("\n\r\x1b[2K[%d] %d suspended %s\n", tmp->n, pid, gv->str);
+//            return;
+//        }
+//        tmp = tmp->next;
+//    }
+//    if (tmp->next == NULL) {
+//        tmp->next = mx_add_empty_job(gv);
+//        tmp->pid = pid;
+//        tmp->str = strdup(gv->str);
+//        gv->last_stoped = tmp;
+//        printf("\n\r\x1b[2K[%d] %d suspended %s\n", tmp->n, pid, gv->str);
+//    }
+//}
